7b6e8dda2d0c64cbdd1d72e8ce6b3b32e4039bf0
CinnyF <47187633+CinnyF@users.noreply.github.com>
2023-07-28 07:14:03 -0500

    Update vis.js

--- vis.js
+++ vis.js
@@ -1,1 +1,20 @@
+$("#to_step2").click(function() {
+    //d3.selectAll("path").remove();
+    innerChart.selectAll("g").remove();
+    hide('#step1');
+    show('#step2');
+})
 
+$("#to_step3").click(function() {
+    //d3.selectAll("path").remove();
+    innerChart.selectAll("g").remove();
+    hide('#step2');
+    show('#step3');
+})
+
+$("#startover").click(function() {
+    innerChart.selectAll("g").remove();
+    hide("#step3");
+    //d3.selectAll("path").remove();
+    show("#step1");
+})
