4fd62c3d646ea1ac64673bccc92163add7ffcce6
CinnyF <47187633+CinnyF@users.noreply.github.com>
2023-07-28 13:18:39 -0500

    Update vis.js

--- vis.js
+++ vis.js
@@ -1,12 +1,11 @@
 const pages = document.querySelectorAll(".page");
-    const translateAmount = 100; 
-    let translate = 0;
+const translateAmount = 100; 
+let translate = 0;
 
-    slide = (direction) => {
-
-      direction === "next" ? translate -= translateAmount : translate += translateAmount;
-
-      pages.forEach(
+slide = (direction) => {
+    
+    direction === "next" ? translate -= translateAmount : translate += translateAmount;
+    pages.forEach(
         pages => (pages.style.transform = `translateX(${translate}%)`)
-      );
-    }
+    );
+}
