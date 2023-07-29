358633a44eac5ee8bf86bf70beab637546fad012
CinnyF <47187633+CinnyF@users.noreply.github.com>
2023-07-29 01:30:49 -0500

    Update vis.js

--- vis.js
+++ vis.js
@@ -1,7 +1,6 @@
 const pages = document.querySelectorAll(".page");
 const translateAmount = 100; 
 let translate = 0;
-
 slide = (direction) => {
     
     direction === "next" ? translate -= translateAmount : translate += translateAmount;
@@ -9,11 +8,3 @@ slide = (direction) => {
         pages => (pages.style.transform = `translateX(${translate}%)`)
     );
 }
-
-
-d3.csv("ALL_DATA_filled_update.csv", function(data) {
-    for (var i = 0; i < data.length; i++) {
-        console.log(data[i].Province_State);
-        console.log("loading data...")
-    }
-});
