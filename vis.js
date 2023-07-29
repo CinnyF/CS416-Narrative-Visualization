a1f55db7f1446717298058791843de45613d21a4
CinnyF <47187633+CinnyF@users.noreply.github.com>
2023-07-29 02:31:42 -0500

    Update vis.js

--- vis.js
+++ vis.js
@@ -10,8 +10,8 @@ slide = (direction) => {
 }
 
 const states = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Diamond Princess", "District of Columbia", "Florida", "Georgia", "Grand Princess", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgin Islands", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-const width = 200;
-const height = 200;
+const width = 500;
+const height = 500;
 
 var x = d3.scaleBand().domain(states).range([0, width]);
 var y = d3.scaleLinear().domain(0,100).range([height, 0]);
