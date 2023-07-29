53d0dc0c6880591b19ac057c3f5839eaf3ae8683
CinnyF <47187633+CinnyF@users.noreply.github.com>
2023-07-29 02:24:14 -0500

    Update vis.js

--- vis.js
+++ vis.js
@@ -9,6 +9,18 @@ slide = (direction) => {
     );
 }
 
-d3.csv("ALL_DATA_filled_update.csv", (d) => {
-    console.log(d)
-})
+const states = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Diamond Princess", "District of Columbia", "Florida", "Georgia", "Grand Princess", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgin Islands", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+
+
+var x = d3.scaleBand().domain(states).range([0, width]);
+var y = d3.scaleLinear().domain(0,100).range([height, 0]);
+ds.select('svg').append('g').attr('transform','translate('+margin+','+margin+')').call(d3.axisLeft(y));
+ds.select('svg').append('g').attr('transform','translate('+margin+','+margin+')').call(d3.axisBottom(x));
+
+
+
+// async function init(type) {
+//     data = await d3.csv("ALL_DATA_filled_update.csv");
+//     d3.select('svg')
+//     .append('g')
+//         .attr('transform','translate('+margin+','+margin+')');
