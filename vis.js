c946e73e979c8558f400be13f378d170f72da040
CinnyF <47187633+CinnyF@users.noreply.github.com>
2023-07-29 14:01:49 -0500

    Update vis.js

--- vis.js
+++ vis.js
@@ -76,7 +76,7 @@ async function init(date) {
             .style("font-size", 8);
 
     var filteredData = data.filter(function(d) { return d.Date == '05-12-2020'; });
-    console.log(filteredData.Confirmed)
+    console.log(filteredData.Confirmed);
 
     svg.selectAll(".bar")
         .data(filteredData)
