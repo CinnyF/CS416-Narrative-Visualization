2a53f72bd496413f887954f1c1a1647cc757f54b
CinnyF <47187633+CinnyF@users.noreply.github.com>
2023-07-29 00:53:01 -0500

    Update vis.js

--- vis.js
+++ vis.js
@@ -9,3 +9,10 @@ slide = (direction) => {
         pages => (pages.style.transform = `translateX(${translate}%)`)
     );
 }
+
+
+d3.csv("ALL_DATA_filled_update.csv", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[i].Province_State);
+    }
+});
