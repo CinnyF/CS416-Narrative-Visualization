41756eaba301207f7031a0a06d9b437030480a26
CinnyF <47187633+CinnyF@users.noreply.github.com>
2023-07-29 03:02:44 -0500

    Update vis.js

--- vis.js
+++ vis.js
@@ -10,14 +10,15 @@ slide = (direction) => {
 }
 
 const states = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Diamond Princess", "District of Columbia", "Florida", "Georgia", "Grand Princess", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgin Islands", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-const width = 500;
-const height = 500;
+const texty = [1,10,100,1000,10000,100000,1000000]
+const width = 900;
+const height = 600;
 const margin = 50;
 
 var x = d3.scaleBand().domain(states).range([0, width]);
-var y = d3.scaleLinear().domain(0,100).range([height, 0]);
-d3.select('svg').append('g').attr('transform','translate('+margin+','+margin+')').call(d3.axisLeft(y));
-d3.select('svg').append('g').attr('transform','translate('+margin+','+margin+')').call(d3.axisBottom(x));
+var y = d3.scaleLinear().domain(texty).range([height, 0]);
+d3.select('svg').append('g').attr('transform','translate('+margin+','+margin+')').call(d3.axisLeft(y)).tickValues([1,10,100,1000,10000,100000,1000000]);
+d3.select('svg').append('g').attr('transform','translate('+margin+','+margin+')').call(d3.axisBottom(x)).attr("transform", "rotate(-90)");
 
 
 
