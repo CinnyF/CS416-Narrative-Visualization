1b2a7091f30294391cfb0a1647a12bd516a0b579
CinnyF <47187633+CinnyF@users.noreply.github.com>
2023-07-30 09:36:59 -0500

    Update vis.js

--- vis.js
+++ vis.js
@@ -119,10 +119,10 @@ async function init(date) {
     console.log(y1(confirmed_avg1))
     console.log("-------")
     svg1.append("line")
-        .attr("x1", 0)
+        .attr("x1", margin)
         .attr("y1", y1(confirmed_avg1))
         .attr("x2", width / 2 + margin)
-        .attr("y2",height / 2)
+        .attr("y2", y1(confirmed_avg1))
         .style("stroke", "black")
 
 
