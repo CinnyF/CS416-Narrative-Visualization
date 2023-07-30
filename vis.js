30585774bed1daec7ac0e5bc9233c1be62102ce1
CinnyF <47187633+CinnyF@users.noreply.github.com>
2023-07-29 21:46:24 -0500

    Update vis.js

--- vis.js
+++ vis.js
@@ -17,35 +17,9 @@ slider.oninput = function() {
   output.innerHTML = calculate_date(this.value);
 }
 
-const states = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Diamond Princess", "District of Columbia", "Florida", "Georgia", "Grand Princess", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgin Islands", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-const texty = [1,10,100,1000,10000,100000,1000000]
 const width = 1100;
 const height = 450;
 const margin = 50;
-
-
-
-// var x = d3.scaleBand().domain(states).range([0, width]);
-// //var y = d3.scaleLinear().domain(texty).range([height, 0]);
-// var y = d3.scaleLinear().domain([0, 23847]).range([height, 0]);
-
-// async function init(date) {
-//     data = await d3.csv("ALL_DATA_filled_organized_2020.csv");
-
-//     d3.select('svg')
-//         .append('g')
-//         .attr('transform','translate('+margin+','+margin+')')
-//        .call(d3.axisLeft(y).tickFormat(d3.format('~s')));
-//     d3.select('svg')
-//         .append('g')
-//         .attr('transform','translate('+margin+','+(height+margin)+')')
-//         .call(d3.axisBottom(x))
-//         .selectAll("text") 
-//             .attr("transform", "translate(-10,10)rotate(-90)")
-//             .style("text-anchor", "end")
-//             .style("font-size", 8);
-
-
 
 async function init(date) {
     data = await d3.csv("ALL_DATA_filled_organized_2020_AS.csv");
@@ -66,31 +40,13 @@ async function init(date) {
     var y = d3.scaleLinear()
         .range([height, 0])
         .domain([0, d3.max(filteredData, function(d) { return d.Confirmed; })]);
-    console.log(d3.max(filteredData, function(d) { return d.Confirmed; }))
-
-
-    
-    // var svg = d3.select('svg')
-    //     .append('g')
-    //     .attr('transform','translate('+margin+','+margin+')');
-
-    // svg.append('g')
-    //    .call(d3.axisLeft(y).tickFormat(d3.format('~s')));
-
-
+    // console.log(d3.max(filteredData, function(d) { return d.Confirmed; }))
     
     var svg = d3.select('svg')
 
     svg.append('g')
        .attr('transform','translate('+margin+','+margin+')')
        .call(d3.axisLeft(y).tickFormat(d3.format('~s')));
-    // svg.append('g')
-    //     .attr("transform", "translate(0," + height + ")")
-    //     .call(d3.axisBottom(x))
-    //     .selectAll("text") 
-    //         .attr("transform", "translate(-10,10)rotate(-90)")
-    //         .style("text-anchor", "end")
-    //         .style("font-size", 8);
 
     svg.append('g')
         .attr('transform','translate('+margin+','+(height+margin)+')')
@@ -100,7 +56,7 @@ async function init(date) {
             .style("text-anchor", "end")
             .style("font-size", 8);
 
-    console.log(filteredData.Confirmed);
+    // console.log(filteredData.Confirmed);
 
     svg.selectAll(".bar")
         .data(filteredData)
@@ -114,27 +70,12 @@ async function init(date) {
     .attr('transform', 'translate(0,' + margin + ')');
 }
 
+// async function update(date) {
+//     d3.select("#demo")
+// }
 
 
 
-
-
-
-// d3.csv("ALL_DATA_filled_organized_2020.csv", function(d) {
-//   console.log(d.Province_State);
-//   return {
-//     year: new Date(d.Date), // convert "Date" column to Date
-//     province_state: d.Province_State,
-//     confirmed: +d.Confirmed, // convert "Confirmed" column to number
-//     deaths: +d.Deaths // convert "Deaths" column to number
-//   };
-// }, function(error, rows) {
-//   console.log(rows);
-// });
-
-// var x = d3.scaleBand().domain(states).range([0, width]);
-// var y = d3.scaleLinear().domain(texty).range([height, 0]);
-// d3.select('svg').append('g').attr('transform','translate('+margin+','+margin+')').call(d3.axisLeft(y));
 
 
 
@@ -214,10 +155,3 @@ function calculate_date(num) {
   }
   return res_date;
 }
-
-
-// async function init(type) {
-//     data = await d3.csv("ALL_DATA_filled_update.csv");
-//     d3.select('svg')
-//     .append('g')
-//         .attr('transform','translate('+margin+','+margin+')');
