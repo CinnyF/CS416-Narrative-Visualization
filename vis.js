a8247d28b2c2cc5157ab5b4052b8dbef94b224c7
CinnyF <47187633+CinnyF@users.noreply.github.com>
2023-07-30 19:41:18 -0500

    Update vis.js

--- vis.js
+++ vis.js
@@ -72,8 +72,8 @@ async function init(date) {
 
     svg1.append('g')
        .attr('transform','translate('+margin+','+margin+')')
-       .call(d3.axisLeft(y1).tickFormat(d3.format('~s')))
-        .tickArguments([5,".0s"]);
+       .call(d3.axisLeft(y1)
+       .tickValues([10,100,1000,10000,100000,1000000, 10000000, 100000000, 1000000000]).tickFormat(d3.format('~s'));
 
     svg1.append('g')
         .attr('transform','translate('+margin+','+(height+margin)+')')
